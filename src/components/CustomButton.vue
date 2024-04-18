60520c1704cfb2c1359cd02097787e4ab46867d1
David-Pena <dev.davidpe@gmail.com>
2024-04-18 00:24:55 -0700

    chore: playing with some components

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="custom-button" @click="handleClick">
+    {{ buttonText }}
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps(["buttonText"]);
+
+const handleClick = () => {
+  console.log("Clicked...");
+};
+</script>
+
+<style scoped>
+.custom-button {
+  /* Your custom button styles */
+  background-color: #007bff;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+/* Additional styles can be added for hover, active, etc. states */
+.custom-button:hover {
+  background-color: #0056b3;
+}
+</style>
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -13,11 +13,63 @@
       </ion-header>
 
       <ExploreContainer name="Tab 1 page" />
+
+      <ion-button id="open-modal" expand="block">Open Sheet Modal</ion-button>
+
+      <ion-modal
+        trigger="open-modal"
+        :is-open="false"
+        :initial-breakpoint="1"
+        :breakpoints="[0, 1]"
+        :handle="false"
+        :can-dismiss="false"
+      >
+        <div class="block">
+          <div class="header">
+            <span>Header</span>
+            <span>Icon</span>
+          </div>
+          <CustomButton button-text="Click me" />
+        </div>
+      </ion-modal>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonModal,
+  IonItem,
+  IonList,
+  IonAvatar,
+  IonImg,
+  IonLabel,
+  IonSearchbar,
+} from "@ionic/vue";
 import ExploreContainer from "@/components/ExploreContainer.vue";
+import CustomButton from "@/components/CustomButton.vue";
 </script>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+}
+.block {
+  width: 100%;
+  height: 300px;
+
+  background-color: orange;
+}
+
+ion-modal {
+  --height: auto;
+}
+</style>
